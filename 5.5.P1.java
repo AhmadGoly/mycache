a29a1e6efaa70446b9be302cb4816a85905d1a6c
Ahmad Golkar <89976272+AhmadGoly@users.noreply.github.com>
2022-12-29 18:55:56 +0330

    Update 5.5.P1.java

--- 5.5.P1.java
+++ 5.5.P1.java
@@ -1,43 +1,28 @@
 public class Mycache {
 
 	public static void main(String[] args) {
-		double hartime=0;
-		double maxhar=0;
-		double waittime=0;
-		double maxwait=0;
-		double idletime=0;
-		double lastfinish=0;
-		
-		double[] between = new double[100];
-		double[] arrive = new double[100];
-		double[] unload = new double[100];
-		double[] finish = new double[100];
-		double[] idle = new double[100];
-		double[] wait = new double[100];
-		double[] start = new double[100];
-		double[] harbor = new double[100];
-		double timediff;
+		double hartime=0;double maxhar=0;double waittime=0;double maxwait=0;
+		double idletime=0;double lastfinish=0;
 		int n = 100;
-		
+		double[] between = new double[n];
+		double[] arrive = new double[n];
+		double[] unload = new double[n];
+		double[] finish = new double[n];
+		double[] idle = new double[n];
+		double[] wait = new double[n];
+		double[] start = new double[n];
+		double[] harbor = new double[n];
+		double timediff;
 		between[1]=15+Math.random()*130;
 		unload[1]=45+Math.random()*45;
-		
 		arrive[1]=between[1];
-		
-		hartime=unload[1];
-		maxhar=unload[1];
-		idletime=arrive[1];
-		
-		finish[1]=arrive[1]+unload[1];
-		
+		hartime=unload[1];maxhar=unload[1];
+		idletime=arrive[1];finish[1]=arrive[1]+unload[1];
 		for(int i =2;i<n;i++) {
 			between[i]=15+Math.random()*130;
 			unload[i]=45+Math.random()*45;
-			
 			arrive[i]=arrive[i-1]+between[i];
-			
 			timediff=arrive[i]-finish[i-1];
-			
 			if(timediff>1) {
 				
 				idle[i]=timediff;
@@ -65,13 +50,13 @@ public class Mycache {
 			if(i==(n-1)) {
 				lastfinish=finish[i];
 			}
-			
 		}
 		hartime=hartime/n;
 		waittime=waittime/n;
 		idletime=idletime/lastfinish;
-		
-		System.out.println("Running for n:"+n+"\nCalculated Hartime is:"+hartime+"\nCalculated Wait time is:"+waittime+"\nCalculated Maxhar is:"+maxhar+"\nCalculated Maxwait is:"+maxwait+"\nCalculated Idletime is:"+idletime);
+		System.out.println("Running for n:"+n+"\nCalculated Hartime is:"+hartime+"\n
+				   Calculated Wait time is:"+waittime+"\nCalculated Maxhar is:"+maxhar+"\n
+				   Calculated Maxwait is:"+maxwait+"\nCalculated Idletime is:"+idletime);
 	}
 	
 
